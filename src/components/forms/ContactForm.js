5f5029b585df527a5e26c27710c9cd0aec93f0eb
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-01-14 17:00:29 -0600

    added recaptua

--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactForm = () => {
   return (
     <div className='container'>
-      <form>
+      <form method="POST" data-netlify="true" data-netlify-recaptcha="true">
         <div className="messages">
           <div className="controls">
             <div className="row">
@@ -49,6 +49,9 @@ const ContactForm = () => {
                   <textarea id="form_message" name="message" className="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please, leave us a message."></textarea>
                   <div className="help-block with-errors"></div>
                 </div>
+              </div>
+              <div className='my-2'>
+                <div data-netlify-recaptcha="true"></div>
               </div>
               <div className='col-md-12'>
                 <input type='submit' className="btn btn-success btn-send" value="Send message" />
